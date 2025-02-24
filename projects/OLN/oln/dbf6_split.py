7d5f2b474228ee546b98d1f910ef100c35cdc3f8
yonafalinie <yona.f.binti-abd-gaus@durham.ac.uk>
2025-02-24 14:10:57 +0000

    Added dbf6 config

---
+++ projects/OLN/oln/dbf6_split.py
@@ -0,0 +1,180 @@
+"""This file contains code to build dataloader of COCO-split dataset.
+
+Reference:
+    "Learning Open-World Object Proposals without Learning to Classify",
+        Aug 2021. https://arxiv.org/abs/2108.06753
+        Dahun Kim, Tsung-Yi Lin, Anelia Angelova, In So Kweon and Weicheng Kuo
+"""
+import os.path as osp
+
+from typing import List, Union
+import copy
+
+from mmengine.fileio import get_local_path
+
+from mmdet.registry import DATASETS
+from mmdet.datasets.coco import CocoDataset
+
+# try:
+#     import pycocotools
+#     if not hasattr(pycocotools, '__sphinx_mock__'):  # for doc generation
+#         assert pycocotools.__version__ >= '12.0.2'
+# except AssertionError:
+#     raise AssertionError('Incompatible version of pycocotools is installed. '
+#                          'Run pip uninstall pycocotools first. Then run pip '
+#                          'install mmpycocotools to install open-mmlab forked '
+#                          'pycocotools.')
+
+
+@DATASETS.register_module()
+class DBF6SplitDataset(CocoDataset):
+
+    def __init__(self, 
+                 is_class_agnostic=False, 
+                 train_class='all',
+                 eval_class='all',
+                 filter_empty_gt=True,
+                 min_size=32,
+                 **kwargs):
+        print(f"Initializing DBF6SplitDataset with train_class={train_class}, eval_class={eval_class}")
+        # We convert all category IDs into 1 for the class-agnostic training and
+        # evaluation. We train on train_class and evaluate on eval_class split.
+        self.is_class_agnostic = is_class_agnostic
+        self.train_class = train_class
+        self.eval_class = eval_class
+        self.filter_empty_gt = filter_empty_gt
+        self.min_size = min_size
+        super(DBF6SplitDataset, self).__init__(**kwargs)
+    
+    CLASSES = ('firearm', 'firearmpart', 'knife', 'camera', 'ceramic_knife', 'laptop')
+    VOC_CLASSES = (
+               'knife', 'camera', 'ceramic_knife', 'laptop')
+    NONVOC_CLASSES = (
+               'firearm', 'firearmpart')
+    class_names_dict = {
+        'all': CLASSES,
+        'voc': VOC_CLASSES,
+        'nonvoc': NONVOC_CLASSES
+    }
+
+    def load_data_list(self) -> List[dict]:
+        """Load annotation from COCO style annotation file.
+
+        Args:
+            ann_file (str): Path of annotation file.
+
+        Returns:
+            list[dict]: Annotation info from COCO api.
+        """
+
+        with get_local_path(
+                self.ann_file, backend_args=self.backend_args) as local_path:
+            self.coco = self.COCOAPI(local_path)
+
+        self.cat_ids = self.coco.get_cat_ids(cat_names=self.metainfo['classes'])
+        self.train_cat_ids = self.coco.get_cat_ids(
+            cat_names=self.class_names_dict[self.train_class]
+            )
+        self.eval_cat_ids = self.coco.get_cat_ids(
+            cat_names=self.class_names_dict[self.eval_class]
+            )
+        if self.is_class_agnostic:
+            self.cat2label = {cat_id: 0 for cat_id in self.cat_ids}
+        else:
+            self.cat2label = {
+                cat_id: i for i, cat_id in enumerate(self.cat_ids)}
+            
+        self.cat_img_map = copy.deepcopy(self.coco.cat_img_map)
+        img_ids = self.coco.get_img_ids()
+        data_list = []
+        total_ann_ids = []
+        for img_id in img_ids:
+            raw_img_info = self.coco.load_imgs([img_id])[0]
+            raw_img_info['img_id'] = img_id
+
+            ann_ids = self.coco.get_ann_ids(img_ids=[img_id])
+            raw_ann_info = self.coco.load_anns(ann_ids)
+            total_ann_ids.extend(ann_ids)
+
+            parsed_data_info = self.parse_data_info({
+                'raw_ann_info':
+                raw_ann_info,
+                'raw_img_info':
+                raw_img_info
+            })
+            data_list.append(parsed_data_info)
+        if self.ANN_ID_UNIQUE:
+            assert len(set(total_ann_ids)) == len(
+                total_ann_ids
+            ), f"Annotation ids in '{self.ann_file}' are not unique!"
+
+        del self.coco
+
+        # print(f"Returning data_list: {data_list}")
+
+        return data_list
+
+    def parse_data_info(self, raw_data_info: dict) -> Union[dict, List[dict]]:
+        """Parse raw annotation to target format.
+
+        Args:
+            raw_data_info (dict): Raw data information load from ``ann_file``
+
+        Returns:
+            Union[dict, List[dict]]: Parsed annotation.
+        """
+        img_info = raw_data_info['raw_img_info']
+        ann_info = raw_data_info['raw_ann_info']
+
+        data_info = {}
+
+        # TODO: need to change data_prefix['img'] to data_prefix['img_path']
+        img_path = osp.join(self.data_prefix['img'], img_info['file_name'])
+        if self.data_prefix.get('seg', None):
+            seg_map_path = osp.join(
+                self.data_prefix['seg'],
+                img_info['file_name'].rsplit('.', 1)[0] + self.seg_map_suffix)
+        else:
+            seg_map_path = None
+        data_info['img_path'] = img_path
+        data_info['img_id'] = img_info['img_id']
+        data_info['seg_map_path'] = seg_map_path
+        data_info['height'] = img_info['height']
+        data_info['width'] = img_info['width']
+
+        if self.return_classes:
+            data_info['text'] = self.metainfo['classes']
+            data_info['caption_prompt'] = self.caption_prompt
+            data_info['custom_entities'] = True
+
+        instances = []
+        for i, ann in enumerate(ann_info):
+            instance = {}
+
+            if ann.get('ignore', False):
+                continue
+            x1, y1, w, h = ann['bbox']
+            inter_w = max(0, min(x1 + w, img_info['width']) - max(x1, 0))
+            inter_h = max(0, min(y1 + h, img_info['height']) - max(y1, 0))
+            if inter_w * inter_h == 0:
+                continue
+            if ann['area'] <= 0 or w < 1 or h < 1:
+                continue
+            if ann['category_id'] not in self.train_cat_ids:
+                continue
+            bbox = [x1, y1, x1 + w, y1 + h]
+
+            if ann.get('iscrowd', False):
+                instance['ignore_flag'] = 1
+                instance['gt_bbox_ignore'] = bbox
+            else:
+                instance['ignore_flag'] = 0
+            instance['bbox'] = bbox
+            instance['bbox_label'] = self.cat2label[ann['category_id']]
+
+            if ann.get('segmentation', None):
+                instance['mask'] = ann['segmentation']
+
+            instances.append(instance)
+        data_info['instances'] = instances
+        return data_info
