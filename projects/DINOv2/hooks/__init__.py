c7d0b4704cdb4604befc353b0570af1cc94d4bd2
yonafalinie <yona.f.binti-abd-gaus@durham.ac.uk>
2025-10-30 16:20:14 +0000

    modify dinov2 init

--- projects/DINOv2/hooks/__init__.py
+++ projects/DINOv2/hooks/__init__.py
@@ -1,3 +1,4 @@
 from .fp16_compression_hook import Fp16CompresssionHook
+from .freeze_backbone_hook import FreezeBackboneHook
 
-__all__ = ['Fp16CompresssionHook']
+__all__ = ['Fp16CompresssionHook', 'FreezeBackboneHook']
